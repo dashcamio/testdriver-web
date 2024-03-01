bc5fa676053a220014901e6547a7eedae8822b50
Ian Jennings <ian@meetjennings.com>
2024-03-01 08:08:57 -0600

    Update prerun.sh

--- .testdriver/prerun.sh
+++ .testdriver/prerun.sh
@@ -1,20 +1,4 @@
 rm ~/Desktop/WITH-LOVE-FROM-AMERICA.txt
-brew install go
-brew tap scripthaus-dev/scripthaus
-brew install scripthaus
-npm install -g yarn
-mkdir ~/build
-cd ~/build
-git clone https://github.com/wavetermdev/waveterm.git
-cd waveterm
-scripthaus run build-backend
-echo "Yarn"
-yarn
-echo "Rebuild"
-scripthaus run electron-rebuild
-echo "Webpack"
-scripthaus run webpack-build
-echo "Starting Electron"
-scripthaus run electron 1>/dev/null 2>&1 &
-echo "Electron Done"
+npm install dashcam-chrome --save
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --load-extension=./node_modules/dashcam-chrome/build/ 1>/dev/null 2>&1 &
 exit
